001c1699ace4056ea6997ba3c2f6fef6d21b158e
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2022-04-23 16:35:13 -0700

    Agregando Clase en App

---
+++ Index.js
@@ -0,0 +1,11 @@
+const MissionCommander = require('./App/MissionComander');
+
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const Alexis = new MissionCommander("Alexis")
+console.log(Alexis.name)
+
+const Daniel = new MissionCommander("Daniel")
+console.log(Daniel.name)
